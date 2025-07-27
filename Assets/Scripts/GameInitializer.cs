f5b30344109143abfe228b1e31adb23996f66c7d
SneakyGoose <luna27l@outlook.com>
2025-07-27 12:11:42 +0200

    modified ui and added start game

--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -4,12 +4,31 @@ public class GameInitializer : MonoBehaviour
 {
     [SerializeField] private InputHandler inputHandler;
     [SerializeField] private PlayerController playerController;
+    [SerializeField] private GameObject playButton;
+    
+    public static GameInitializer Instance {get; private set;}
+    public bool GameStarted { get; private set; } = false;
+
+    private void Awake()
+    {
+        Instance = this;
+        Time.timeScale = 0f;
+    }
 
     void Start()
     {
-        inputHandler.OnSwipeLeft += playerController.MoveLeft;
-        inputHandler.OnSwipeRight += playerController.MoveRight;
-        inputHandler.OnDoubleTap += playerController.Dash;
-        inputHandler.OnSwipeUp += playerController.Jump;
+        inputHandler.OnSwipeLeft += () => { if (GameStarted) playerController.MoveLeft(); };
+        inputHandler.OnSwipeRight += () => { if (GameStarted) playerController.MoveRight(); };
+        inputHandler.OnDoubleTap += () => { if (GameStarted) playerController.Dash(); };
+        inputHandler.OnSwipeUp += () => { if (GameStarted) playerController.Jump(); };
+    }
+    
+    public void StartGame()
+    {
+        GameStarted = true;
+        Time.timeScale = 1f;
+
+        if (playButton != null)
+            playButton.SetActive(false);
     }
 }
